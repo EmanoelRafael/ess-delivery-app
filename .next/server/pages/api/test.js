e32b21ca8313ebc51720175312f8aaf8fe2aacc9
Erick de Cássio <ecjr@cin.ufpe.br>
2022-08-25 10:10:19 -0300

    Instalação do next, front e back end rodando

---
+++ .next/server/pages/api/test.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/test";
+exports.ids = ["pages/api/test"];
+exports.modules = {
+
+/***/ "(api)/./pages/api/test.js":
+/*!***************************!*\
+  !*** ./pages/api/test.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ test)\n/* harmony export */ });\nfunction test(req, res) {\n    res.json(\"test\");\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9wYWdlcy9hcGkvdGVzdC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQWUsU0FBU0EsSUFBSSxDQUFDQyxHQUFHLEVBQUVDLEdBQUcsRUFDckM7SUFDSUEsR0FBRyxDQUFDQyxJQUFJLENBQUMsTUFBTSxDQUFDLENBQUM7Q0FDcEIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lc3MtZGVsaXZlcnktYXBwLy4vcGFnZXMvYXBpL3Rlc3QuanM/NmU3YSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiB0ZXN0KHJlcSwgcmVzKVxyXG57XHJcbiAgICByZXMuanNvbihcInRlc3RcIik7XHJcbn1cclxuXHJcbiJdLCJuYW1lcyI6WyJ0ZXN0IiwicmVxIiwicmVzIiwianNvbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(api)/./pages/api/test.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./pages/api/test.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home()
+{
+    return <h1>Qualquer coisa</h1>;
+}
+
+export default Home;
