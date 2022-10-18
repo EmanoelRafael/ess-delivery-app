91408035cf14d0d38472392318c94ce3f1cf92b3
vitinho souza <32920217+vbmsouza@users.noreply.github.com>
2022-10-18 13:08:53 -0300

    Create acompedidos.component.ts

---
+++ ta-gui/src/app/acompedidos/acompedidos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acompedidos',
+  templateUrl: './acompedidos.component.html',
+  styleUrls: ['./acompedidos.component.css']
+})
+export class AcompedidosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
